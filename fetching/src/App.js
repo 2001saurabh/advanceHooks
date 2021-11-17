17bd08047a3cda6b34d0d50ee950bd258078dac6
Saurabh Pathak <4saurabhpathak@gmail.com>
2021-11-17 16:50:29 +0530

    updated fetching

--- fetching/src/App.js
+++ fetching/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import DatafetchingOne from "./DatafetchingOne";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DatafetchingOne />
+    </div>
+  );
 }
 
 export default App;
--- fetching/src/DatafetchingOne.js
+++ fetching/src/DatafetchingOne.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function DatafetchingOne() {
-  return <div></div>;
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/2")
+      .then((res) => {
+        setLoading(false);
+        setPost(res.data);
+        setError("");
+        console.log(res);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setPost({});
+        setError("Server not responding!");
+      });
+  }, []);
+  return (
+    <div>
+      {isLoading ? (
+        <div class="ui active centered inline loader"></div>
+      ) : (
+        <div className="ui purple header">{post.title}</div>
+      )}
+    </div>
+  );
 }
 
-export default DatafetchingOn;
+export default DatafetchingOne;
