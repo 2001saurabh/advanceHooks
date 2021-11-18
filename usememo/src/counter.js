daff06a8bbb8d2c7014663e60524ca7c71c47b35
Saurabh Pathak <4saurabhpathak@gmail.com>
2021-11-18 13:38:01 +0530

    memo

---
+++ usememo/src/counter.js
@@ -0,0 +1,33 @@
+import React, { useMemo, useState } from "react";
+
+function Counter() {
+  const [countOne, setCountOne] = useState(0);
+  const [countTwo, setcountTwo] = useState(0);
+
+  const incrementOne = () => {
+    setCountOne(countOne + 1);
+  };
+
+  const incrementTwo = () => {
+    setcountTwo(countTwo + 1);
+  };
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 200000000) i++;
+    return countOne % 2 === 0;
+  }, [countOne]);
+  return (
+    <div className=" ui segment container">
+      <span className=" ui header container">{isEven ? "Even" : "odd"}</span>
+      <button className="ui blue button " onClick={incrementOne}>
+        Increment Counter One :- {countOne}
+      </button>
+
+      <button className="ui blue button " onClick={incrementTwo}>
+        Increment Counter Two :- {countTwo}
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
